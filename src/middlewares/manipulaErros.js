fc818a08542fdc5d563e8e1587512c5be9f3fccf
hendrix <leandrotully@gmail.com>
2024-01-20 17:06:00 -0300

    Erros de validação

--- src/middlewares/manipulaErros.js
+++ src/middlewares/manipulaErros.js
@@ -5,8 +5,12 @@ function manipulaErros(error, req, res, next) {
   console.log(error)
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).json({ success: false, message: "Código inválido" })
+  } else if (error instanceof mongoose.Error.ValidationError) {
+    const msgError = Object.values(error.errors).map(error => error.message).join(";")
+    res.status(400).send({ success: false, message: `Erros apresentados: ${msgError}` })
+  } else {
+    res.status(500).json({ success: false, message: "Erro no Servidor" })
   }
-  res.status(500).json({ success: false, message: "Erro no Servidor" })
 }
 
 export default manipulaErros
--- src/models/Autor.js
+++ src/models/Autor.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 
 const autorSchema = new mongoose.Schema(
-    {
-        id: { type: String },
-        nome: { type: String, required: true },
-        origem: { type: String }
-    },
-    {
-        versionKey: false
-    }
+  {
+    id: { type: String },
+    nome: { type: String, required: [true, "Nome do Autor(a) é obrigatório."] },
+    origem: { type: String }
+  },
+  {
+    versionKey: false
+  }
 )
 
 const autores = mongoose.model("autores", autorSchema)
